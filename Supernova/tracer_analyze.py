d7cf722ffac522d3515e314a9d06d21bb5ee2fc4
Dario <dariozarcone@gmail.com>
2020-04-24 02:19:11 +0200

    Add solver test

--- Supernova/tracer_analyze.py
+++ Supernova/tracer_analyze.py
@@ -37,24 +37,39 @@ plt.rcParams.update(params)
 
 
 # Data analysis
-## Choose tracer
-if sys.argv[1] not in ["1","2"]:
+## Choose mode and tracer
+if sys.argv[1] not in ["conv", "solv"]:
+    raise ValueError("Plot arg must be\n \
+                     'conv' for convergence test\n \
+                     'solv' for solver test")
+else:
+    modekey = sys.argv[1]
+if sys.argv[2] not in ["1","2"]:
     raise ValueError("Tracer arg must be 1 or 2")
 else:
-    tracer = "tr"+sys.argv[1]
+    tracer = "tr"+sys.argv[2]
 ## Setup units
 UNIT_LENGTH = 3.0856776e18
 UNIT_DENSITY = 1.6726219e-24
 M_SUN = 1.98855e33
 ## Load data
-dim = ["128", "256", "512", "1024"]
+modedict= {
+    'conv':{'files' :["./128/roe/", "./256/roe/", "./512/roe/", "./1024/roe/"],
+            'labels':["128x128", "256x256", "512x512", "1024x1024"],
+            'lim'   :{"tr1":[0.60,1.02], "tr2":[0.92,1.02]}},
+    'solv':{'files' :["./512/hll/", "./512/hllc/", "./512/roe/"],
+            'labels':["hll", "hllc", "roe"],
+            'lim'   :{"tr1":[0.80,1.025], "tr2":[0.97,1.01]}}
+}
+
+mode = modedict[modekey]
 data = {}
 mass = {}
-for val in dim:
+for val,path in zip(mode['labels'], mode['files']):
     data.update({val:[]})
     mass.update({val:[]})
     for n in range(0, 11):
-        data[val].append(pp.pload(n, w_dir="./{}/".format(val)))
+        data[val].append(pp.pload(n, w_dir=path))
     ## Calc volume
     vol = np.zeros([data[val][0].n1, data[val][0].n2])
     for i in range(data[val][0].n2):
@@ -74,30 +89,21 @@ fig, ax = plt.subplots()
 styleaxes(ax)
 ax.set_xlabel(r'Step temporale')
 ax.set_ylabel(r'Massa normalizzata con M(0)')
-lim = {"tr1":[0.60,1.02], "tr2":[0.92,1.02]}
-ax.set_ylim(lim[tracer])
+ax.set_ylim(mode['lim'][tracer])
 ax.set_xlim([0,10])
 ax.xaxis.set_major_locator(MultipleLocator(1))
 
 ## Plot mass for 128x128, 256x256, 512x512 and 1024x1024
 ## Normalized
-for val in dim:
-    ax.plot(mass[val]/mass[val][0], label="{}x{}".format(val, val))
+for val in mode['labels']:
+    ax.plot(mass[val]/mass[val][0], label=val)
 
-## Add line at inital mass and add legend
-initMass = {"tr1":10,
-            "tr2":2*np.pi*np.pi*0.5*0.5*2.5*1.e2
-            *UNIT_DENSITY
-            *UNIT_LENGTH**3
-            /M_SUN}
-ax.axhline(y=initMass[tracer]/mass[val][0], color="C4")
-ax.text(4, initMass[tracer]/mass[val][0] + 0.002, "Massa condizioni iniziali")
 ax.legend(fancybox=True, loc=0,
           title='Massa di elementi con tracciante {} > 90% \n normalizzata a M(0)'.format(tracer),
           framealpha=1)
 
 # Show and save plot
 plt.tight_layout()
-fig.savefig("andamentoMassa{}.pdf".format(tracer), 
+fig.savefig("{}-test-{}.pdf".format(modekey, tracer), 
             bbox_inches="tight")
 plt.show()
